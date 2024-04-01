78f01f7a6765c31762d8abb0eda7826ac412f752
sahilborse18 <sahilborse.909098@gmail.com>
2024-04-01 23:10:00 +0530

    Initial commit

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div>
+           {/* <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" /> */}
+           <img src="https://static.vecteezy.com/system/resources/previews/034/099/682/large_2x/proper-education-icon-concept-for-learning-and-knowledge-flat-illustration-free-vector.jpg" alt="" />
+           
+    </div>
+  )
+}
